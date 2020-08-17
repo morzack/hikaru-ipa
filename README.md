8eca0da2d653c170f99f2fd129e4d3a0e565894e
morzack <jckesle2@ncsu.edu>
2020-08-17 00:04:46 -0400

    Add IPA-fr-ascii and fix button selection

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Also, kudos to [@NickOveracker](https://github.com/NickOveracker) and his [merem
 
 This is effectively a variant of a site like [Real Kana](https://realkana.com) except designed to work with the International Phonetic Alphabet (IPA).
 
-- IPA ASCII is derived from ["The ASCII Phonetic Alphabet"](http://www.antimoon.com/how/pronunc-ascii.htm)
+- IPA English ASCII is derived from ["The ASCII Phonetic Alphabet"](http://www.antimoon.com/how/pronunc-ascii.htm)
 - [IPA French](https://en.wikipedia.org/wiki/Help:IPA/French) and [IPA English](https://en.wikipedia.org/wiki/Help:IPA/English) are modelled after their respective Wikipedia pages
+- IPA French ASCII is an abomination constructed from the [Branner symbol set](https://en.wikipedia.org/wiki/Comparison_of_ASCII_encodings_of_the_International_Phonetic_Alphabet#Symbols)
 
 [Project site](https://morzack.github.io/hikaru-ipa/)
 
--- main.js
+++ main.js
@@ -2,7 +2,22 @@ var charData = [
     {
         id: "ipa-fr",
         name: "IPA French",
-        chars: []
+        chars: [{char: "", value: ""}]
+    },
+    {
+        id: "ipa-fr-ascii",
+        name: "IPA French ASCII",
+        chars: [{char: "b", value: "b"}, {char: "d", value: "d"}, {char: "f", value: "f"}, {char: "g", value: "g"},
+                {char: "k", value: "k"}, {char: "l", value: "l"}, {char: "m", value: "m"}, {char: "n", value: "n"},
+                {char: "ɲ", value: "nj)"}, {char: "ŋ", value: "ng)"}, {char: "p", value: "p"}, {char: "ʁ", value: "R%"},
+                {char: "s", value: "s"}, {char: "ʃ", value: "S"}, {char: "t", value: "t"}, {char: "v", value: "v"},
+                {char: "z", value: "z"}, {char: "ʒ", value: "3\""}, {char: "j", value: "j"}, {char: "w", value: "w"},
+                {char: "ɥ", value: "h&"}, {char: "a", value: "a"}, {char: "ɑ", value: "a\""}, {char: "e", value: "e"},
+                {char: "ɛ", value: "E"}, {char: "ɛ:", value: "E:"}, {char: "ə", value: "@"}, {char: "i", value: "i"},
+                {char: "œ", value: "oe)"}, {char: "ø", value: "o/)"}, {char: "o", value: "o"}, {char: "ɔ", value: "c&"},
+                {char: "u", value: "u"}, {char: "y", value: "y"}, {char: "ɑ̃", value: "a\"~"}, {char: "ɛ̃", value: "E~"},
+                {char: "œ̃", value: "oe)~"}, {char: "ɔ̃", value: "c&~"}
+            ]
     },
     {
         id: "ipa-en",
@@ -10,9 +25,20 @@ var charData = [
         chars: []
     },
     {
-        id: "ipa-ascii",
-        name: "IPA ASCII",
-        chars: [{char: "ʌ", value: "^"}, {char: "ɑ:", value: "a:"}, {char: "æ", value: "@"}, {char: "e", value: "e"}, {char: "ə", value: ".."}, {char: "ɜ:ʳ", value: "e:(r)"}, {char: "ɪ", value: "i"}, {char: "i:", value: "i:"}, {char: "ɒ", value: "o"}, {char: "ɔ:", value: "o:"}, {char: "ʊ", value: "u"}, {char: "u:", value: "u:"}, {char: "aɪ", value: "ai"}, {char: "aʊ", value: "au"}, {char: "eɪ", value: "ei"}, {char: "oʊ", value: "Ou"}, {char: "ɔɪ", value: "oi"}, {char: "eəʳ", value: "e..(r)"}, {char: "ɪəʳ", value: "i..(r)"}, {char: "ʊəʳ", value: "u..(r)"}, {char: "b", value: "b"}, {char: "d", value: "d"}, {char: "f", value: "f"}, {char: "g", value: "g"}, {char: "h", value: "h"}, {char: "j", value: "j"}, {char: "k", value: "k"}, {char: "l", value: "l"}, {char: "m", value: "m"}, {char: "n", value: "n"}, {char: "ŋ", value: "N"}, {char: "p", value: "p"}, {char: "r", value: "r"}, {char: "s", value: "s"}, {char: "ʃ", value: "S"}, {char: "t", value: "t"}, {char: "tʃ", value: "tS"}, {char: "θ", value: "th"}, {char: "ð", value: "TH"}, {char: "v", value: "v"}, {char: "w", value: "w"}, {char: "z", value: "z"}, {char: "ʒ", value: "Z"}, {char: "dʒ", value: "dZ"}]
+        id: "ipa-en-ascii",
+        name: "IPA English ASCII",
+        chars: [{char: "ʌ", value: "^"}, {char: "ɑ:", value: "a:"}, {char: "æ", value: "@"}, {char: "e", value: "e"},
+                {char: "ə", value: ".."}, {char: "ɜ:ʳ", value: "e:(r)"}, {char: "ɪ", value: "i"}, {char: "i:", value: "i:"},
+                {char: "ɒ", value: "o"}, {char: "ɔ:", value: "o:"}, {char: "ʊ", value: "u"}, {char: "u:", value: "u:"},
+                {char: "aɪ", value: "ai"}, {char: "aʊ", value: "au"}, {char: "eɪ", value: "ei"}, {char: "oʊ", value: "Ou"},
+                {char: "ɔɪ", value: "oi"}, {char: "eəʳ", value: "e..(r)"}, {char: "ɪəʳ", value: "i..(r)"}, {char: "ʊəʳ", value: "u..(r)"},
+                {char: "b", value: "b"}, {char: "d", value: "d"}, {char: "f", value: "f"}, {char: "g", value: "g"},
+                {char: "h", value: "h"}, {char: "j", value: "j"}, {char: "k", value: "k"}, {char: "l", value: "l"},
+                {char: "m", value: "m"}, {char: "n", value: "n"}, {char: "ŋ", value: "N"}, {char: "p", value: "p"},
+                {char: "r", value: "r"}, {char: "s", value: "s"}, {char: "ʃ", value: "S"}, {char: "t", value: "t"},
+                {char: "tʃ", value: "tS"}, {char: "θ", value: "th"}, {char: "ð", value: "TH"}, {char: "v", value: "v"},
+                {char: "w", value: "w"}, {char: "z", value: "z"}, {char: "ʒ", value: "Z"}, {char: "dʒ", value: "dZ"}
+            ]
     }
 ]
 
@@ -44,26 +70,29 @@ let help = document.getElementById("help");
 let options = document.getElementsByClassName("options");
 let gameover = document.getElementsByClassName("gameover");
 
-for(const ipaCat of charData) {
-    var btnAdd = document.createElement("BUTTON");
-    btnAdd.innerHTML = ipaCat.name;
-    btnAdd.id = ipaCat.id;
-    btnAdd.className = "options";
+for(var ipaCat of charData) {
+    (function (ipaCat) {
+        var btnAdd = document.createElement("BUTTON");
+        btnAdd.innerHTML = ipaCat.name;
+        btnAdd.id = ipaCat.id;
+        btnAdd.className = "options";
 
-    btnAdd.addEventListener("click", ()=>{
-        if(enabled.includes(ipaCat.id)){
-            const index = enabled.indexOf(ipaCat.id);
-            if (index > -1) {
-                enabled.splice(index, 1)
+        btnAdd.addEventListener("click", ()=>{
+            var id = ipaCat.id;
+            if(enabled.includes(id)){
+                const index = enabled.indexOf(id);
+                if (index > -1) {
+                    enabled.splice(index, 1)
+                }
+                btnAdd.style.setProperty('background-color', '#363636');
+            } else {
+                enabled.push(id);
+                btnAdd.style.setProperty('background-color', '#6665d2');
             }
-            btnAdd.style.setProperty('background-color', '#363636');
-        } else {
-            enabled.push(ipaCat.id);
-            btnAdd.style.setProperty('background-color', '#6665d2');
-        }
-    });
+        });
 
-    buttonBox.appendChild(btnAdd);
+        buttonBox.appendChild(btnAdd);
+    })(ipaCat);
 }
 
 function helpClicked(char){
